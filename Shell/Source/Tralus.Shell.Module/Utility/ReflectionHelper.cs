30e1841d263ab0f9943fb0a60953688fbc87b16b
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-03-06 00:58:37 +0330

    Correcting assembly loading

--- Shell/Source/Tralus.Shell.Module/Utility/ReflectionHelper.cs
+++ Shell/Source/Tralus.Shell.Module/Utility/ReflectionHelper.cs
@@ -39,30 +39,34 @@ namespace Tralus.Shell.Module.Utility
 
         private static void LoadModule(string moduleInfosStr, List<Type> tempImprtedModule, List<Type> tempDbContext)
         {
-            Assembly moduleCustomAssembly;
             var moduleFullPath = moduleInfosStr + ".Module";
             var moduleAssembly = Assembly.Load(moduleFullPath);
             var dataFullPath = moduleInfosStr + ".Data";
             var dataAssembly = Assembly.Load(dataFullPath);
 
-            // ToDo: This should remove from here and move to a web related place. This project should not have any reference to web.
-            if (System.Web.HttpRuntime.AppDomainAppId != null)
-            {
-                var moduleWwebFullPath = moduleInfosStr + ".Module.Web";
-                moduleCustomAssembly = Assembly.Load(moduleWwebFullPath);
-            }
-            else
-            {
-                var moduleWinFullPath = moduleInfosStr + ".Module.Win";
-                moduleCustomAssembly = Assembly.Load(moduleWinFullPath);
-            }
-
-
             var modules = moduleAssembly.GetTypes().Where(t => t.IsSubclassOf(typeof(TralusModule)));
-            var modulesSpecific = moduleCustomAssembly.GetTypes().Where(
-                t => t.IsSubclassOf(typeof(TralusModule)) && !t.FullName.Contains("TralusWinModule") && !t.FullName.Contains("TralusWebModule"));
 
-            tempImprtedModule.AddRange(modules.Concat(modulesSpecific));
+            // ToDo: This should remove from here and move to a web related place. This project should not have any reference to web.
+
+            //if (moduleInfosStr != "Tralus.Framework")
+            //{
+                Assembly moduleCustomAssembly;
+                if (System.Web.HttpRuntime.AppDomainAppId != null)
+                {
+                    var moduleWwebFullPath = moduleInfosStr + ".Module.Web";
+                    moduleCustomAssembly = Assembly.Load(moduleWwebFullPath);
+                }
+                else
+                {
+                    var moduleWinFullPath = moduleInfosStr + ".Module.Win";
+                    moduleCustomAssembly = Assembly.Load(moduleWinFullPath);
+                }
+
+                var modulesSpecific = moduleCustomAssembly.GetTypes().Where(
+                    t => t.IsSubclassOf(typeof(TralusModule)) && !t.FullName.Contains("TralusWinModule") && !t.FullName.Contains("TralusWebModule"));
+
+                tempImprtedModule.AddRange(modules.Concat(modulesSpecific));
+            //}
 
             tempDbContext.AddRange(dataAssembly.GetTypes().Where(
                 t => t.IsSubclassOf(typeof(DbContextBase)) && !t.IsAbstract && !t.FullName.Contains("DbContextBase"))
