028ff8e74730123a770df4aecc37ae9c4e36709d
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-03-06 00:28:05 +0330

    Changed the assembly loading method

--- Shell/Source/Tralus.Shell.Web/Global.asax.cs
+++ Shell/Source/Tralus.Shell.Web/Global.asax.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Web.Configuration;
 using System.Web;
 
@@ -11,29 +15,67 @@ using DevExpress.Web;
 using Tralus.Framework.BusinessModel.Entities;
 using Tralus.Shell.Module.Security;
 using Tralus.Shell.Web.Security;
+using ReflectionHelper = Tralus.Shell.Module.Utility.ReflectionHelper;
 
-namespace Tralus.Shell.Web {
-    public class Global : System.Web.HttpApplication {
-        public Global() {
+namespace Tralus.Shell.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static IEnumerable<Type> LoadedModuleTypes;
+        public static IEnumerable<Type> LoadedContextTypes;
+
+        public Global()
+        {
             InitializeComponent();
+
             if (ConfigurationManager.AppSettings["ApplicationStyle"] == "New")
                 WebApplication.Instance.SwitchToNewStyle();
+
+            try
+            {
+                AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+                {
+                    var location = Path.Combine(AppDomain.CurrentDomain.RelativeSearchPath,
+                        args.Name.Split(',')[0] + ".dll");
+                    if (File.Exists(location))
+                    {
+                        var assembly = Assembly.LoadFrom(location);
+                        return assembly;
+                    }
+                    return null;
+                };
+
+                ReflectionHelper.GetImportedModules(out LoadedModuleTypes, out LoadedContextTypes);
+                //foreach (var loadedModuleType in LoadedModuleTypes)
+                //{
+                //    var loadedModule = (ModuleBase)Activator.CreateInstance(loadedModuleType);
+                //    Modules.Insert(0, loadedModule);
+                //}
+            }
+            catch(Exception exception)
+            {
+                Trace.WriteLine($"Unable to load modules: {exception}");
+            }
+
         }
-        protected void Application_Start(Object sender, EventArgs e) {
+        protected void Application_Start(Object sender, EventArgs e)
+        {
             ASPxWebControl.CallbackError += new EventHandler(Application_Error);
 #if EASYTEST
             DevExpress.ExpressApp.Web.TestScripts.TestScriptsManager.EasyTestEnabled = true;
 #endif
         }
-        protected void Session_Start(Object sender, EventArgs e) {
+        protected void Session_Start(Object sender, EventArgs e)
+        {
             var webApplication = new ShellAspNetApplication();
             ConfigSecurity(webApplication);
-            
+
             WebApplication.SetInstance(Session, webApplication);
 
 
 
-            if(ConfigurationManager.ConnectionStrings["Default"] != null) {
+            if (ConfigurationManager.ConnectionStrings["Default"] != null)
+            {
                 WebApplication.Instance.ConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
             }
 #if EASYTEST
@@ -41,7 +83,7 @@ namespace Tralus.Shell.Web {
                 WebApplication.Instance.ConnectionString = ConfigurationManager.ConnectionStrings["EasyTestConnectionString"].ConnectionString;
             }
 #endif
-            
+
             //WebApplication.Instance.Security = new TralusSecurityStrategy(new AdfsAuthentication());
             WebApplication.Instance.Setup();
             WebApplication.Instance.Start();
@@ -89,30 +131,37 @@ namespace Tralus.Shell.Web {
 
         }
 
-        protected void Application_BeginRequest(Object sender, EventArgs e) {
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
         }
-        protected void Application_EndRequest(Object sender, EventArgs e) {
+        protected void Application_EndRequest(Object sender, EventArgs e)
+        {
         }
-        protected void Application_AuthenticateRequest(Object sender, EventArgs e) {
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
         }
-        protected void Application_Error(Object sender, EventArgs e) {
+        protected void Application_Error(Object sender, EventArgs e)
+        {
             ErrorHandling.Instance.ProcessApplicationError();
         }
-        protected void Session_End(Object sender, EventArgs e) {
+        protected void Session_End(Object sender, EventArgs e)
+        {
             WebApplication.LogOff(Session);
             WebApplication.DisposeInstance(Session);
         }
-        protected void Application_End(Object sender, EventArgs e) {
+        protected void Application_End(Object sender, EventArgs e)
+        {
         }
         #region Web Form Designer generated code
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
         }
         #endregion
     }
 
-   
+
 }
