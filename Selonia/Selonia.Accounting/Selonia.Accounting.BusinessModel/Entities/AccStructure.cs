e04103dd3b29d5f712c8337002cb61a0f30663f5
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-05-01 23:31:23 +0430

    Add AccStructure

---
+++ Selonia/Selonia.Accounting/Selonia.Accounting.BusinessModel/Entities/AccStructure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base.General;
+using Tralus.Framework.BusinessModel.Entities;
+
+namespace Selonia.Accounting.BusinessModel.Entities
+{
+    public abstract class AccStructure : EntityBase, IHierarchyEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public abstract IHierarchyEntity Parent { get; set; }
+
+        ITreeNode IHCategory.Parent
+        {
+            get { return Parent; }
+            set { Parent = (IHierarchyEntity)value; }
+        }
+        ITreeNode ITreeNode.Parent => Parent;
+
+        public abstract IBindingList Children { get; }
+
+        public virtual Image GetImage(out string imageName)
+        {
+            imageName = "BO_Category";
+            return ImageLoader.Instance.GetImageInfo(imageName).Image;
+        }
+    }
+}
