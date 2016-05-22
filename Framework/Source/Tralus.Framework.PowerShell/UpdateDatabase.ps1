a4455e805792b79c24df64fd38b34e82937b05c4
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-05-23 00:40:42 +0430

    Create a prescript for updating database.

---
+++ Framework/Source/Tralus.Framework.PowerShell/UpdateDatabase.ps1
@@ -0,0 +1,7 @@
+﻿cd (Join-Path $HOME 'TralusLocal\win')
+Import-Module .\Tralus.Framework.PowerShell.dll
+
+$toMigrate = @("Tralus.Framework.Migration")
+$connectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=Tralus;Integrated Security=True;App=Tralus"
+
+Update-TralusDatabase -MigrationAssemblies $toMigrate -ConnectionString $connectionString -WhatIf -Verbose
