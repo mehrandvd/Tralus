c483a1fd2b467b632884ac03809068bde05b7bf8
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-05-29 23:29:35 +0430

    EntityScript added.

---
+++ Framework/Source/Tralus.Framework.Module/Controllers/RunEntityScriptViewController.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Tralus.Framework.Module.Controllers
+{
+    partial class RunEntityScriptViewController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RunEntityScriptAction = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // RunEntityScriptAction
+            // 
+            this.RunEntityScriptAction.Caption = "Run Scripts";
+            this.RunEntityScriptAction.ConfirmationMessage = null;
+            this.RunEntityScriptAction.Id = "8d092d92-248b-4a2a-94f3-2b54b6b8cc3d";
+            this.RunEntityScriptAction.ToolTip = null;
+            this.RunEntityScriptAction.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.RunEntityScriptAction_Execute);
+            // 
+            // RunEntityScriptViewController
+            // 
+            this.Actions.Add(this.RunEntityScriptAction);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction RunEntityScriptAction;
+    }
+}
