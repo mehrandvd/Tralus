b2f15f80e31e51eb9dbacf3a0ef21132f8ca579c
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-06-04 18:19:32 +0430

    Missing DbContextExtensions

---
+++ Framework/Source/Tralus.Framework.BusinessModel/Entities/DbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Tralus.Framework.BusinessModel.Entities
+{
+    public static class DbContextExtensions
+    {
+        public static void AddIfNotExists<T>(this DbSet<T> dbSet, T entity) where T : EntityBase
+        {
+            if (!dbSet.Any(e => e.Id == entity.Id))
+            {
+                dbSet.Add(entity);
+            }
+        }
+
+        public static void AddRangeIfNotExists<T>(this DbSet<T> dbSet, IEnumerable<T> entities) where T : EntityBase
+        {
+            foreach (var entity in entities)
+            {
+                AddIfNotExists(dbSet, entity);
+            }
+        }
+    }
+}
