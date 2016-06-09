3cb47602f8a958bd6e5686ced95f5348e4365468
Mehran Davoudi <mehran.davoudi@hotmail.com>
2016-06-10 00:52:33 +0430

    Add map module.

--- Framework/Source/Tralus.Framework.Module.Web/TralusWebModule.cs
+++ Framework/Source/Tralus.Framework.Module.Web/TralusWebModule.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Maps.Web;
 
 namespace Tralus.Framework.Module.Web
 {
     public class TralusWebModule : TralusModule
     {
+        public TralusWebModule():base()
+        {
+            RequiredModuleTypes.Add(typeof(MapsAspNetModule));
+        }
     }
 }
