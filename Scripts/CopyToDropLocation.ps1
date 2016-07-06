91d7e7548a414f8072c743208d0b3fad32c4d8b9
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-07-07 01:41:05 +0430

    Add files via upload

--- Scripts/CopyToDropLocation.ps1
+++ Scripts/CopyToDropLocation.ps1
@@ -10,27 +10,37 @@ param(
 
     [Parameter(Mandatory = $true)]
     [string]
-    $buildConfiguration
+    $buildConfiguration,
 
+    [Parameter(Mandatory = $true)]
+    [string[]]
+    $includeFileNames,
+
+    [Parameter(Mandatory = $true)]
+    [string[]]
+    $includeFileTypes
 )
 
-$fileTypes = $("*.exe","*.dll","*.exe.config","*.config","*.pdb","*.xml")
+#$includeFileTypes = "*.exe","*.dll","*.exe.config","*.config","*.xml"
+#$includeFileNames = "*framework*", "*shell*"
 $binFolders = $("*bin*")
 
 
 
-$dllfiles = Get-ChildItem  $sourceLocation -recurse  -Include $binFolders | 
+$applicationfiles = Get-ChildItem  $sourceLocation -recurse  -Include $binFolders | 
             ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include $buildConfiguration } |
-            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include "Melkradar*.dll" }
+            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include $includeFileNames } |
+            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include $includeFileTypes } 
 
 $pdbfiles = Get-ChildItem  $sourceLocation -recurse  -Include $binFolders | 
             ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include $buildConfiguration } |
-            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include "Melkradar*.pdb" }
+            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include $includeFileNames } |
+            ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -include "*.pdb" }
 
 
 if(-not (Test-Path $dropLocationPath)) {
     New-Item -Path $dropLocationPath -ItemType Directory | Out-Null
 }
 
-$dllfiles | Copy-Item -Destination $dropLocationPath -Recurse -Force
+$applicationfiles | Copy-Item -Destination $dropLocationPath -Recurse -Force
 $pdbfiles | Copy-Item -Destination $dropLocationPath -Recurse -Force
