eb8f269379af68d1ddf57257979d4f0901e81c1c
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-07-19 02:09:24 +0430

    Add PersianDateControl Win version

--- Framework/Source/Tralus.Framework.Module.Win/Controls/BaseControls/PersianDateControl.cs
+++ Framework/Source/Tralus.Framework.Module.Win/Controls/BaseControls/PersianDateControl.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.Utils.Design;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraPrinting.Native;
+using Tralus.Framework.Module.Win.Editors;
 
 namespace Tralus.Framework.Module.Win.Controls.BaseControl
 {
-    public partial class PersianDateControl : UserControl
+    [DefaultBindingProperty("DateTime")]
+    public partial class PersianDateControl : BaseEdit
     {
         private EventHandler onValueChanged;
 
@@ -19,18 +26,24 @@ namespace Tralus.Framework.Module.Win.Controls.BaseControl
             InitializeComponent();
         }
 
-        
-        [Bindable(true,BindingDirection.TwoWay)]
+        [Bindable(true, BindingDirection.TwoWay)]
         public DateTime? Value
         {
             get { return dateTimeSelector.Value; }
             set { dateTimeSelector.Value = value; }
         }
 
-        public string Text
+
+        public override string Text
         {
             get { return dateTimeSelector.Text; }
             set { dateTimeSelector.Text = value; }
+        }
+
+        public override object EditValue
+        {
+            get { return Value; }
+            set { Value = (DateTime?)value; }
         }
 
         private void dateTimeSelector_ValueChanged(object sender, EventArgs e)
@@ -52,12 +65,42 @@ namespace Tralus.Framework.Module.Win.Controls.BaseControl
 
         protected virtual void OnValueChanged(EventArgs e)
         {
-
             // Call the event handler
-            if (onValueChanged != null)
+            onValueChanged?.Invoke(this, e);
+            CompleteChanges();
+            var cancelEventArgs = new CancelEventArgs();
+            OnValidating(cancelEventArgs);
+            OnValidated(e);
+        }
+
+        [Browsable(false)]
+        public override string EditorTypeName
+        {
+            get
             {
-                onValueChanged(this, e);
+                return RepositoryItemPersianDate.CustomEditName;
             }
         }
+
+        [DXCategory("Properties")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [SmartTagSearchNestedProperties]
+        public RepositoryItemPersianDate Properties => this.fProperties as RepositoryItemPersianDate;
+
+        protected override void OnValidated(EventArgs e)
+        {
+            base.OnValidated(e);
+        }
+
+        protected override void OnValidating(CancelEventArgs e)
+        {
+            base.OnValidating(e);
+        }
+
+        protected override void OnValidatingCore(CancelEventArgs e)
+        {
+            base.OnValidatingCore(e);
+        }
     }
+
 }
---
+++ Framework/Source/Tralus.Framework.Module.Win/Editors/PersianDateEditor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.Utils.Design;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Registrator;
+using DevExpress.XtraEditors.Repository;
+using Tralus.Framework.Module.Win.Controls.BaseControl;
+
+namespace Tralus.Framework.Module.Win.Editors
+{
+    [PropertyEditor(typeof(DateTime),  true)]
+    public class PersianDateEditor : WinPropertyEditor, IInplaceEditSupport
+    {
+        private PersianDateControl control;
+        public PersianDateEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model)
+        {
+        }
+
+        static PersianDateEditor()
+        {
+            RepositoryItemPersianDate.RegisterPersianDateEditor();
+        }
+
+        protected override object CreateControlCore()
+        {
+            control = new PersianDateControl();
+            control.ValueChanged += control_ValueChanged;
+            this.ControlBindingProperty = "Value";
+            return control;
+        }
+
+        private void control_ValueChanged(object sender, EventArgs e)
+        {
+            OnControlValueChanged();
+        }
+
+        public RepositoryItem CreateRepositoryItem()
+        {
+            return new RepositoryItemPersianDate();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (control != null)
+            {
+                control.ValueChanged -= control_ValueChanged;
+                control = null;
+            }
+            base.Dispose(disposing);
+        }
+        
+        protected override void OnControlCreating()
+        {
+            base.OnControlCreating();
+        }
+     
+           
+    }
+}
