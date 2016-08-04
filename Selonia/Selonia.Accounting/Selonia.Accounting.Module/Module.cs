01bf74a216ecf5f55b291e22f008e0c089e861b3
Mehran <Mehran@MEHRAN-LAPTOP>
2016-08-04 21:19:16 +0430

    Removing additional overrided methods in module

--- Selonia/Selonia.Accounting/Selonia.Accounting.Module/Module.cs
+++ Selonia/Selonia.Accounting/Selonia.Accounting.Module/Module.cs
@@ -21,25 +21,25 @@ using Tralus.Framework.Module;
 namespace Selonia.Accounting.Module {
     // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppModuleBasetopic.aspx.
     public sealed partial class AccountingModule : TralusModule {
-        static AccountingModule() {
-            DevExpress.Data.Linq.CriteriaToEFExpressionConverter.SqlFunctionsType = typeof(System.Data.Entity.SqlServer.SqlFunctions);
-			DevExpress.Data.Linq.CriteriaToEFExpressionConverter.EntityFunctionsType = typeof(System.Data.Entity.DbFunctions);
-            // Uncomment this code to delete and recreate the database each time the data model has changed.
-            // Do not use this code in a production environment to avoid data loss.
-            // #if DEBUG
-            // Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AccountingDbContext>());
-            // #endif 
-        }
+   //     static AccountingModule() {
+   //         DevExpress.Data.Linq.CriteriaToEFExpressionConverter.SqlFunctionsType = typeof(System.Data.Entity.SqlServer.SqlFunctions);
+			//DevExpress.Data.Linq.CriteriaToEFExpressionConverter.EntityFunctionsType = typeof(System.Data.Entity.DbFunctions);
+   //         // Uncomment this code to delete and recreate the database each time the data model has changed.
+   //         // Do not use this code in a production environment to avoid data loss.
+   //         // #if DEBUG
+   //         // Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AccountingDbContext>());
+   //         // #endif 
+   //     }
         public AccountingModule() {
             InitializeComponent();
         }
-        public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
-            ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
-            return new ModuleUpdater[] { updater };
-        }
-        public override void Setup(XafApplication application) {
-            base.Setup(application);
-            // Manage various aspects of the application UI and behavior at the module level.
-        }
+        //public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        //    ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        //    return new ModuleUpdater[] { updater };
+        //}
+        //public override void Setup(XafApplication application) {
+        //    base.Setup(application);
+        //    // Manage various aspects of the application UI and behavior at the module level.
+        //}
     }
 }
