12bca9f7b7d661e6bb4b476bf85d65f3010b571d
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-09-06 21:36:05 +0430

    Update InvokeClickOnceAppBuilder.ps1

--- Scripts/Deployment Script/InvokeClickOnceAppBuilder.ps1
+++ Scripts/Deployment Script/InvokeClickOnceAppBuilder.ps1
@@ -38,7 +38,7 @@
         $manifestFilePath = Join-Path $destinationPath "$ApplicationName.manifest"
         $deploymentFilePath = Join-Path $destinationPath "$ApplicationName.application"
 
-        $providerUrl = "$DeploymentUrl/$Version"
+        $providerUrl = "$DeploymentUrl/$Version/$ApplicationName.application"
         
         if (-not (Test-Path $destinationPath)) {
             if ($PSCmdlet.ShouldProcess($destinationPath, "Create Destination Path")) {
