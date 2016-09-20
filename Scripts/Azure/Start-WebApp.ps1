254def4c2dbaac2d0a1ce95ed38b7b4e541bea21
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-09-20 19:07:45 +0430

    Add Azure WebApp Stop&Start PowerShell Script

---
+++ Scripts/Azure/Start-WebApp.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$True)]
+    [string]$WebSiteName,
+
+    [Parameter(Mandatory=$False)]
+    [string]$Slot
+)
+
+$WebSite = Get-AzureRmWebApp -Name $WebSiteName
+$ResourceGroupName = $WebSite.ResourceGroup
+
+if($Slot)
+{
+    Start-AzureRmWebAppSlot -Name $WebSiteName -ResourceGroupName $ResourceGroupName -Slot $Slot | Out-Null
+    Write-Host "Started slot '$Slot' belonging to web app '$WebSiteName'"
+}
+else
+{
+    Start-AzureRmWebApp -Name $WebSiteName -ResourceGroupName $ResourceGroupName | Out-Null
+    Write-Host "Started web app '$WebSiteName'"
+}
