9a8ac7280b0fd4728513e9b5ad44b421baf9d0a0
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-09-27 00:02:39 +0330

    Add Create,Start and Stop Windows Service for use in release management

---
+++ Scripts/Azure/Create-WindowsService.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+
+Param
+(
+    # Param1 help description
+    [Parameter(Mandatory=$true,
+               Position=0)]
+    [String]
+    $ServiceName,
+
+    [Parameter(Mandatory=$true,
+               Position=1)]
+    [String]
+    $BinaryPathName
+)
+
+$service = Get-Service $ServiceName -ErrorAction SilentlyContinue
+
+if (!$service)
+{
+   New-Service -Name $ServiceName -BinaryPathName $BinaryPathName -StartupType Automatic
+}
+    
