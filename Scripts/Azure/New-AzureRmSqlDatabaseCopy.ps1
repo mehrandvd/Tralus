38c7157e964fbadc3b4168251c8ebdac4da949ed
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-10-05 01:54:17 +0330

    Add Azure SQL Database Copy and Remove

---
+++ Scripts/Azure/New-AzureRmSqlDatabaseCopy.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $ResourceGroupName, 
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $ServerName,
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $DatabaseName,
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $CopyServerName,
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $CopyDatabaseName,
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $CopyResourceGroupName
+)
+
+Write-Host $ResourceGroupName
+Write-Host $ServerName
+Write-Host $DatabaseName
+Write-Host $CopyResourceGroupName
+Write-Host $CopyServerName
+Write-Host $CopyDatabaseName
+
+New-AzureRmSqlDatabaseCopy -ResourceGroupName $ResourceGroupName  -ServerName $ServerName -DatabaseName $DatabaseName  -CopyServerName $CopyServerName  -CopyDatabaseName $CopyDatabaseName -CopyResourceGroupName $CopyResourceGroupName -Verbose
---
+++ Scripts/Azure/Set-AzureRmSqlDatabaseEdition.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [String] $ResourceGroupName,
+    
+    [Parameter(Mandatory=$true)] 
+    [String] $ServerName,
+
+    [Parameter(Mandatory=$true)]
+    [String] $DatabaseName,
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $Edition
+
+)
+
+Write-Host $ResourceGroupName
+Write-Host $ServerName
+Write-Host $DatabaseName
+Write-Host $Edition
+
+Set-AzureRmSqlDatabase -ResourceGroupName $ResourceGroupName  -ServerName $ServerName  -DatabaseName $DatabaseName -Edition $Edition
