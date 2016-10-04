7ba50eeacf432d0a21907ca338776977aef69bf5
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2016-10-05 01:54:17 +0330

    Add Azure SQL Database Copy and Remove

---
+++ Scripts/Azure/Remove-AzureRmSqlDatabase.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+Param(
+
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $ResourceGroupName, 
+    
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $ServerName,
+    
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $DatabaseName
+)
+
+Write-Host $ResourceGroupName
+Write-Host $ServerName
+Write-Host $DatabaseName
+
+Remove-AzureRmSqlDatabase -ResourceGroupName $ResourceGroupName  -ServerName $ServerName  -DatabaseName $DatabaseName -Confirm -Force
