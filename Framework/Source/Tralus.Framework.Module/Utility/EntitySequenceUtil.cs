92c557c8c10361e3f7571e25d531dcdca9b20a07
Mehran <Mehran@MEHRAN-LAPTOP>
2017-04-21 19:31:13 +0430

    add GetNextChunk

--- Framework/Source/Tralus.Framework.Module/Utility/EntitySequenceUtil.cs
+++ Framework/Source/Tralus.Framework.Module/Utility/EntitySequenceUtil.cs
@@ -88,16 +88,16 @@ namespace Tralus.Framework.Module.Utility
                     throw new Exception($"Sequence is not defined: {sequenceName}");
 
                 var sameSequenceItems = db.Set<EntitySequenceItem>()
-                    .Where(si=>
+                    .Where(si =>
                         si.Param1 == param1 &&
                         si.Param2 == param2 &&
                         si.Param3 == param3 &&
                         si.Param4 == param4 &&
                         si.Param5 == param5);
 
-                var maxValue = 
-                    sameSequenceItems.Any()?
-                    sameSequenceItems.Max(si => si.SeqValue):
+                var maxValue =
+                    sameSequenceItems.Any() ?
+                    sameSequenceItems.Max(si => si.SeqValue) :
                     0;
 
                 var esi = new EntitySequenceItem(true)
@@ -118,6 +118,73 @@ namespace Tralus.Framework.Module.Utility
                 db.SaveChanges();
 
                 return esi.SeqValue;
+            }
+        }
+
+        public static long GetNextChunk(
+            string sequenceName,
+            long chunkSize,
+            string param1 = null,
+            string param2 = null,
+            string param3 = null,
+            string param4 = null,
+            string param5 = null,
+            string usedFor = null
+            )
+        {
+            using (var db = new FrameworkDbContext())
+            {
+                var seq = Cache.GetOrAdd(sequenceName, (seqName) =>
+                {
+                    var s = db.Set<EntitySequence>().FirstOrDefault(es => es.Name == seqName);
+
+                    //if (s == null)
+                    //{
+                    //    var temp = db.Set<EntitySequence>().Create();
+                    //    temp.Name = seqName;
+                    //    temp.Description = $"Automatically created for: {usedFor}";
+                    //    db.SaveChanges();
+                    //}
+                    return s;
+                });
+
+                if (seq == null)
+                    throw new Exception($"Sequence is not defined: {sequenceName}");
+
+                lock (seq)
+                {
+                    var sameSequenceItems = db.Set<EntitySequenceItem>()
+                        .Where(si =>
+                            si.Param1 == param1 &&
+                            si.Param2 == param2 &&
+                            si.Param3 == param3 &&
+                            si.Param4 == param4 &&
+                            si.Param5 == param5);
+
+                    var maxValue =
+                        sameSequenceItems.Any() ?
+                        sameSequenceItems.Max(si => si.SeqValue) :
+                        0;
+
+                    var esi = new EntitySequenceItem(true)
+                    {
+                        SeqValue = maxValue + chunkSize,
+                        EntitySequenceId = seq.Id,
+                        CreationDateTime = DateTime.Now,
+                        Param1 = param1,
+                        Param2 = param2,
+                        Param3 = param3,
+                        Param4 = param4,
+                        Param5 = param5,
+                        UsedFor = usedFor
+                    };
+
+
+                    db.Set<EntitySequenceItem>().Add(esi);
+                    db.SaveChanges();
+
+                    return esi.SeqValue;
+                }
             }
         }
     }
